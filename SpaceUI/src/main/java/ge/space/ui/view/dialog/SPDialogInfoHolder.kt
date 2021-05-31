657b5284262d21987a5fd7740742d84c8bdb3c04
nikantro888 <nikoloz.khidesheli@space.ge>
2021-05-31 13:10:56 +0400

    # Testing Lambda function in dialogs as click events

---
+++ SpaceUI/src/main/java/ge/space/ui/view/dialog/SPDialogInfoHolder.kt
@@ -0,0 +1,12 @@
+package ge.space.ui.view.dialog
+
+import android.os.Parcelable
+import ge.space.ui.view.dialog.view.SPDialogBottomVerticalButton
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class SPDialogInfoHolder(
+        val labelTxt:String,
+        val buttonType: SPDialogBottomVerticalButton.BottomButtonType,
+        val clickEvent: (() -> Unit?)? = null
+): Parcelable
--- SpaceUI/src/main/java/ge/space/ui/view/dialog/SPInfoDialog.kt
+++ SpaceUI/src/main/java/ge/space/ui/view/dialog/SPInfoDialog.kt
@@ -6,12 +6,12 @@ import android.view.View
 import androidx.core.os.bundleOf
 import androidx.core.view.isGone
 import androidx.fragment.app.FragmentActivity
-import ge.space.ui.view.dialog.base.SPBaseDialogBuilder
 import ge.space.spaceui.databinding.SpInfoDialogBinding
 import ge.space.ui.util.extension.argument
 import ge.space.ui.util.extension.nonNullArgument
 import ge.space.ui.util.extension.visibleOrGone
 import ge.space.ui.view.dialog.base.SPBaseDialog
+import ge.space.ui.view.dialog.base.SPBaseDialogBuilder
 import ge.space.ui.view.dialog.view.SPDialogBottomButtonLayout
 import ge.space.ui.view.dialog.view.SPDialogBottomVerticalButton
 
@@ -23,7 +23,7 @@ import ge.space.ui.view.dialog.view.SPDialogBottomVerticalButton
  * @property label allows to set the second title
  * @property iconVisible describes a dialog icon visibility
  * @property labelVisible describes the dialog second title visibility
- * @property buttonPairs describes dialog bottom buttons
+ * @property buttonObjects describes dialog bottom buttons
  * @property isButtonsMultiple sets the dialog bottom buttons multiple flag
  * @property buttonsVisible describes the dialog bottom buttons visibility
  * @property clickAction is triggered when a button of the dialog is triggered
@@ -44,12 +44,13 @@ class SPInfoDialog : SPBaseDialog<SpInfoDialogBinding>() {
 
     private val isButtonsMultiple: Boolean by nonNullArgument(KEY_MULTIPLE, false)
 
-    private val buttonPairs: Array<Pair<String, String>> by nonNullArgument(KEY_BUTTONS, arrayOf())
+    private val buttonObjects: Array<SPDialogInfoHolder> by nonNullArgument(BUTTON_OBJECT, arrayOf())
+
 
     private var clickAction: ((String) -> Unit)? = null
 
     override fun getViewBinding(): SpInfoDialogBinding =
-        SpInfoDialogBinding.inflate(LayoutInflater.from(context))
+            SpInfoDialogBinding.inflate(LayoutInflater.from(context))
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -58,12 +59,13 @@ class SPInfoDialog : SPBaseDialog<SpInfoDialogBinding>() {
         handleVisibility()
         setClicksHandle()
         setButtons()
+
     }
 
     /**
      * Sets an action which is triggered when a button of the dialog is clicked
      */
-    fun onButtonsClick(action: (String) -> Unit) : SPInfoDialog {
+    fun onButtonsClick(action: (String) -> Unit): SPInfoDialog {
         clickAction = action
         return this
     }
@@ -96,40 +98,41 @@ class SPInfoDialog : SPBaseDialog<SpInfoDialogBinding>() {
         if (buttonsVisible) {
             with(binding.lytDialogLinear) {
                 lytButtons.setBottomButtons(
-                    convertDialogButtonsType()
+                        convertDialogButtonsType()
                 )
             }
         }
     }
 
-    private fun convertDialogButtonsType() : SPDialogBottomButtonLayout.SPDialogBottomButton =
-        if (isButtonsMultiple) {
-            SPDialogBottomButtonLayout.SPDialogBottomButton.SPDialogBottomButtonMultiple(
-                getMultipleButtons()
-            )
-        } else {
-            SPDialogBottomButtonLayout.SPDialogBottomButton.SPDialogBottomButtonTwice(
-                createDialogButtonModel(buttonPairs[LEFT_PAIR_INDEX]),
-                createDialogButtonModel(buttonPairs[RIGHT_PAIR_INDEX])
-            )
-        }
+    private fun convertDialogButtonsType(): SPDialogBottomButtonLayout.SPDialogBottomButton =
+            if (isButtonsMultiple) {
+                SPDialogBottomButtonLayout.SPDialogBottomButton.SPDialogBottomButtonMultiple(
+                        getMultipleButtons()
+                )
+            } else {
+                SPDialogBottomButtonLayout.SPDialogBottomButton.SPDialogBottomButtonTwice(
+                        createDialogButtonModel(buttonObjects[LEFT_PAIR_INDEX]),
+                        createDialogButtonModel(buttonObjects.get(RIGHT_PAIR_INDEX)),
+                )
+            }
 
-    private fun getMultipleButtons() : List<SPDialogBottomButtonLayout.SPDialogBottomButtonModel> {
+    private fun getMultipleButtons(): List<SPDialogBottomButtonLayout.SPDialogBottomButtonModel> {
         val buttons = mutableListOf<SPDialogBottomButtonLayout.SPDialogBottomButtonModel>()
-        buttonPairs.forEach {
+        buttonObjects.forEach {
             buttons.add(
-                createDialogButtonModel(it)
+                    createDialogButtonModel(it)
             )
         }
 
         return buttons
     }
 
-    private fun createDialogButtonModel(buttonPair: Pair<String, String>) =
-        SPDialogBottomButtonLayout.SPDialogBottomButtonModel(
-            buttonPair.first,
-            SPDialogBottomVerticalButton.BottomButtonType.valueOf(buttonPair.second)
-        )
+    private fun createDialogButtonModel(buttonObj: SPDialogInfoHolder) =
+            SPDialogBottomButtonLayout.SPDialogBottomButtonModel(
+                    buttonObj.labelTxt,
+                    SPDialogBottomVerticalButton.BottomButtonType.valueOf(buttonObj.buttonType.toString()),
+                    buttonObj.clickEvent
+            )
 
     override fun setDismissAction() {
         binding.lytRoot.setOnClickListener {
@@ -146,6 +149,7 @@ class SPInfoDialog : SPBaseDialog<SpInfoDialogBinding>() {
         const val KEY_BUTTONS_VISIBLE = "KEY_BUTTONS_VISIBLE"
         const val KEY_MULTIPLE = "KEY_MULTIPLE"
         const val KEY_BUTTONS = "KEY_BUTTONS"
+        const val BUTTON_OBJECT = "BUTTON_OBJECT"
 
         private const val LEFT_PAIR_INDEX = 0
         private const val RIGHT_PAIR_INDEX = 1
@@ -157,7 +161,7 @@ class SPInfoDialog : SPBaseDialog<SpInfoDialogBinding>() {
      * Builder class which allows to create [SPInfoDialog]
      */
     class SPInfoDialogBuilder(
-        activity: FragmentActivity
+            activity: FragmentActivity
     ) : SPBaseDialogBuilder<SPInfoDialog>(activity) {
 
         private var title: String? = null
@@ -167,7 +171,8 @@ class SPInfoDialog : SPBaseDialog<SpInfoDialogBinding>() {
         private var infoIconVisible: Boolean = true
         private var buttonsVisible: Boolean = true
         private var isMultiple: Boolean = false
-        private var buttons: Array<Pair<String, String>> = arrayOf()
+        private var buttons: Array<SPDialogInfoHolder> = arrayOf()
+
 
         /**
          * Defines data for the top title
@@ -229,39 +234,39 @@ class SPInfoDialog : SPBaseDialog<SpInfoDialogBinding>() {
          * possibilities to add both right button and left one.
          */
         fun setButtons(
-            multiple: Boolean = false,
-            vararg pairs: Pair<String, SPDialogBottomVerticalButton.BottomButtonType>
+                multiple: Boolean = false,
+                buttons: Array<SPDialogInfoHolder>
         ): SPInfoDialogBuilder {
             this.isMultiple = multiple
 
-            if (!isMultiple && pairs.count() < MINIMUM_TWICE_BUTTONS) {
+            if (!isMultiple && buttons.count() < MINIMUM_TWICE_BUTTONS) {
                 throw IllegalStateException("Pairs parameter has to contain at least $MINIMUM_TWICE_BUTTONS elements for using Twice button type")
             }
 
-            this.buttons = convertPairToParcelable(*pairs)
+            this.buttons = buttons
             return this
         }
 
         private fun convertPairToParcelable(
-            vararg pairs: Pair<String, SPDialogBottomVerticalButton.BottomButtonType>
-        ) : Array<Pair<String, String>> =
-            pairs.toList().map { Pair(it.first, it.second.toString()) }.toTypedArray()
+                vararg pairs: Pair<String, SPDialogBottomVerticalButton.BottomButtonType>
+        ): Array<Pair<String, String>> =
+                pairs.toList().map { Pair(it.first, it.second.toString()) }.toTypedArray()
 
         /**
          * Builds [SPInfoDialog] by using properties with keys
          */
         override fun build(): SPInfoDialog =
-            SPInfoDialog().apply {
-                arguments = bundleOf(
-                    KEY_TITLE to this@SPInfoDialogBuilder.title,
-                    KEY_LABEL to this@SPInfoDialogBuilder.label,
-                    KEY_INFO_ICON_VISIBLE to this@SPInfoDialogBuilder.infoIconVisible,
-                    KEY_TITLE_VISIBLE to this@SPInfoDialogBuilder.titleVisible,
-                    KEY_LABEL_VISIBLE to this@SPInfoDialogBuilder.labelVisible,
-                    KEY_BUTTONS_VISIBLE to this@SPInfoDialogBuilder.buttonsVisible,
-                    KEY_MULTIPLE to this@SPInfoDialogBuilder.isMultiple,
-                    KEY_BUTTONS to this@SPInfoDialogBuilder.buttons
-                )
-            }
+                SPInfoDialog().apply {
+                    arguments = bundleOf(
+                            KEY_TITLE to this@SPInfoDialogBuilder.title,
+                            KEY_LABEL to this@SPInfoDialogBuilder.label,
+                            KEY_INFO_ICON_VISIBLE to this@SPInfoDialogBuilder.infoIconVisible,
+                            KEY_TITLE_VISIBLE to this@SPInfoDialogBuilder.titleVisible,
+                            KEY_LABEL_VISIBLE to this@SPInfoDialogBuilder.labelVisible,
+                            KEY_BUTTONS_VISIBLE to this@SPInfoDialogBuilder.buttonsVisible,
+                            KEY_MULTIPLE to this@SPInfoDialogBuilder.isMultiple,
+                            BUTTON_OBJECT to this@SPInfoDialogBuilder.buttons,
+                    )
+                }
     }
 }
