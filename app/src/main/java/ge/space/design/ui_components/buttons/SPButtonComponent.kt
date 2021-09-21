8264914273078e50039d5a2a5b25e690b0153b2f
Vitalii Syvolotskyi <v.syvolotskyi@space.ge>
2021-09-21 13:12:38 +0300

    created fullwidth button

--- app/src/main/java/ge/space/design/ui_components/buttons/SPButtonComponent.kt
+++ app/src/main/java/ge/space/design/ui_components/buttons/SPButtonComponent.kt
@@ -3,6 +3,7 @@ package ge.space.design.ui_components.buttons
 import com.example.spacedesignsystem.R
 import ge.space.design.main.ShowCaseComponent
 import ge.space.design.ui_components.buttons.default_button.SPDefaultButtonsComponent
+import ge.space.design.ui_components.buttons.fullwidth.SPFullWidthButtonsComponent
 import ge.space.design.ui_components.buttons.horizontal_button.SPHorizontalButtonsComponent
 import ge.space.design.ui_components.buttons.vertical_button.SPVerticalButtonsComponent
 
@@ -16,7 +17,8 @@ class SPButtonComponent: ShowCaseComponent{
         return listOf(
             SPDefaultButtonsComponent(),
             SPVerticalButtonsComponent(),
-            SPHorizontalButtonsComponent()
+            SPHorizontalButtonsComponent(),
+                SPFullWidthButtonsComponent()
         )
     }
 }
