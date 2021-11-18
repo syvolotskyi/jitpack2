72264c242287a79af19ae67be463dc2884c9c660
Artem Krizhanovskiy <a.krizhanovskiy@space.ge>
2021-11-18 11:55:44 +0300

    Updated code related to app theme change

--- app/src/main/java/ge/space/design/main/util/SPSimpleShowActivityExt.kt
+++ app/src/main/java/ge/space/design/main/util/SPSimpleShowActivityExt.kt
@@ -1,6 +1,5 @@
 package ge.space.design.main.util
 
-import androidx.appcompat.app.AlertDialog
 import com.example.spacedesignsystem.R
 import ge.space.design.main.ui.SPBaseActivity
 import ge.space.ui.components.dialogs.data.SPDialogIcon
@@ -8,27 +7,6 @@ import ge.space.ui.components.dialogs.data.SPDialogInfo
 import ge.space.ui.components.dialogs.data.SPDialogInfoHolder
 import ge.space.ui.components.dialogs.dialog_buttons.SPDialogBottomVerticalButton
 import ge.space.ui.components.dialogs.showMultipleButtonDialog
-
-fun SPBaseActivity.showThemeAlertDialog() {
-    AlertDialog.Builder(this) // Window is leaking sometimes via 'this' object
-        .setTitle("Choose theme")
-        .setSingleChoiceItems(
-            arrayOf("Space Dark","Space Light"),
-            preferencesManager.getInt(SPBaseActivity.PREFERENCES_THEME, 0)
-        ) { dialog, which ->
-            preferencesManager.edit()?.apply {
-                putInt(SPBaseActivity.PREFERENCES_THEME, which)
-                apply()
-            }
-            dialog.dismiss()
-            recreate()
-        }.show()
-}
-
-private enum class AppTheme {
-    DARK,
-    LIGHT
-}
 
 fun SPBaseActivity.showThemeCustomDialog() {
     fun updateTheme(theme: AppTheme) {
