a6ab647bfda250f66c2a89f015ab68573f6ad7e0
Artem Krizhanovskiy <a.krizhanovskiy@space.ge>
2021-12-13 14:48:21 +0300

    Added binding to view

--- SpaceUI/src/main/java/ge/space/ui/components/controls/SPToggleIcon.kt
+++ SpaceUI/src/main/java/ge/space/ui/components/controls/SPToggleIcon.kt
@@ -3,9 +3,11 @@ package ge.space.ui.components.controls
 import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
 import ge.space.spaceui.R
+import ge.space.spaceui.databinding.SpToggleIconLayoutBinding
 import ge.space.ui.base.SPBaseView
 import ge.space.ui.base.SPSetViewStyleInterface
 
@@ -15,12 +17,17 @@ class SPToggleIcon @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0
 ) : SPBaseView(context, attrs, defStyleAttr), SPSetViewStyleInterface {
 
+    val binding: SpToggleIconLayoutBinding
+
     var toggleState: ToggleState = ToggleState.DISABLED
     set(value) {
         field = value
         toggle()
     }
 
+    init {
+        binding = SpToggleIconLayoutBinding.inflate(LayoutInflater.from(context), this)
+    }
 
     override fun setViewStyle(@StyleRes newStyle: Int) {
         val styleAttrs = context.theme.obtainStyledAttributes(newStyle, R.styleable.SPToggleIcon)
