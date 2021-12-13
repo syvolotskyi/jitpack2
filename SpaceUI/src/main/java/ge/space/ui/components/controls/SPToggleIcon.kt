bb0f7b7d2328fd3a41e552d3f5ab2e6519573edb
Artem Krizhanovskiy <a.krizhanovskiy@space.ge>
2021-12-13 13:12:37 +0300

    ToggleIcon styles intro

--- SpaceUI/src/main/java/ge/space/ui/components/controls/SPToggleIcon.kt
+++ SpaceUI/src/main/java/ge/space/ui/components/controls/SPToggleIcon.kt
@@ -1,8 +1,11 @@
 package ge.space.ui.components.controls
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import ge.space.spaceui.R
 import ge.space.ui.base.SPBaseView
 import ge.space.ui.base.SPSetViewStyleInterface
 
@@ -12,9 +15,32 @@ class SPToggleIcon @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0
 ) : SPBaseView(context, attrs, defStyleAttr), SPSetViewStyleInterface {
 
+    var toggleState: ToggleState = ToggleState.DISABLED
+    set(value) {
+        field = value
+        toggle()
+    }
 
 
-    override fun setViewStyle(newStyle: Int) {
+    override fun setViewStyle(@StyleRes newStyle: Int) {
+        val styleAttrs = context.theme.obtainStyledAttributes(newStyle, R.styleable.SPToggleIcon)
+        with(styleAttrs) {
+            styledAttributes()
+            recycle()
+        }
+    }
 
+    private fun TypedArray.styledAttributes() {
+
+    }
+
+    private fun toggle() {
+        toggleState = if (toggleState == ToggleState.DISABLED)
+            ToggleState.ENABLED else ToggleState.DISABLED
+    }
+
+    enum class ToggleState {
+        DISABLED,
+        ENABLED
     }
 }
