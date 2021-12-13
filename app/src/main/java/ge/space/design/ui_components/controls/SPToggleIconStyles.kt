eceb6ce7831d6db9656cd875fc33032767d3f936
Artem Krizhanovskiy <a.krizhanovskiy@space.ge>
2021-12-13 15:18:34 +0300

    Added default version of a showcase

---
+++ app/src/main/java/ge/space/design/ui_components/controls/SPToggleIconStyles.kt
@@ -0,0 +1,10 @@
+package ge.space.design.ui_components.controls
+
+data class SPToggleIconSupportsLoading(val styleId: Int)
+
+object SPToggleIconStyles {
+    /*val list = listOf(
+        SPToggleIconSupportsLoading(R.style.SPToggleIcon_Unselected),
+        SPToggleIconSupportsLoading(R.style.SPToggleIcon_Selected)
+    )*/
+}
