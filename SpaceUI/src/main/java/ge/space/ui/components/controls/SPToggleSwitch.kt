b378905bee244360b6f1a87f522023a67921a9fb
Artem Krizhanovskiy <a.krizhanovskiy@space.ge>
2021-12-16 14:06:49 +0300

    Initial style settings of SPToggleSwitch

--- SpaceUI/src/main/java/ge/space/ui/components/controls/SPToggleSwitch.kt
+++ SpaceUI/src/main/java/ge/space/ui/components/controls/SPToggleSwitch.kt
@@ -3,21 +3,32 @@ package ge.space.ui.components.controls
 import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.core.content.withStyledAttributes
 import com.google.android.material.switchmaterial.SwitchMaterial
+import ge.space.spaceui.R
+import ge.space.ui.base.SPBaseView.Companion.DEFAULT_OBTAIN_VAL
 import ge.space.ui.base.SPSetViewStyleInterface
 
 class SPToggleSwitch @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
+    @AttrRes defStyleAttr: Int = 0
 ) : SwitchMaterial(context, attrs, defStyleAttr), SPSetViewStyleInterface {
 
     init {
-
+        getContext().withStyledAttributes(attrs, R.styleable.SPToggleSwitch, defStyleAttr) {
+            setViewStyle(getResourceId(R.styleable.SPToggleSwitch_style, DEFAULT_OBTAIN_VAL))
+            withStyledAttributes()
+        }
     }
 
-    override fun setViewStyle(newStyle: Int) {
-        TODO("Not yet implemented")
+    override fun setViewStyle(@StyleRes newStyle: Int) {
+        context.theme.obtainStyledAttributes(newStyle, R.styleable.SPToggleSwitch).run {
+            withStyledAttributes()
+            recycle()
+        }
     }
 
     private fun TypedArray.withStyledAttributes() {
