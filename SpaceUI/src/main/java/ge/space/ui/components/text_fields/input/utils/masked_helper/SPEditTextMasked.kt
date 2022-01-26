a7225e7baddbd03bac1d01b9e6bdcf94dfd3a3e9
vitalii <syvolotskyi@zfort.com>
2022-01-26 14:36:30 +0200

    Removed possibility to tap "X" into mask edit text

--- SpaceUI/src/main/java/ge/space/ui/components/text_fields/input/utils/masked_helper/SPEditTextMasked.kt
+++ SpaceUI/src/main/java/ge/space/ui/components/text_fields/input/utils/masked_helper/SPEditTextMasked.kt
@@ -365,7 +365,7 @@ open class SPEditTextMasked : AppCompatEditText, TextWatcher {
 
     companion object {
         const val SPACE = " "
-        const val ALLOWED_CHARS = "1234567890 /X"
+        const val ALLOWED_CHARS = "1234567890"
         const val CHAR_REPRESENTATION = "#"
         const val MASK_SYMBOL = "X"
     }
