e3777e86b54c301d1793e59f20a871e1a5c9cf0e
v.syvolotskyi <v.syvolotskyi@space.ge>
2022-07-06 10:55:22 +0300

    # Added result listener

--- app/src/main/java/ge/space/design/ui_components/text_fields/dropdown/SPExampleFragment.kt
+++ app/src/main/java/ge/space/design/ui_components/text_fields/dropdown/SPExampleFragment.kt
@@ -5,14 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ge.space.ui.util.extension.onClick
 import com.example.spacedesignsystem.R
+import com.example.spacedesignsystem.databinding.SpExampleFragmentLayoutBinding
+import ge.space.spaceui.databinding.SpTabNavigationChildViewLayoutBinding
+import ge.space.ui.components.dropdowns.strategy.SPBottomSheetResultListener
 
-class SPExampleFragment : Fragment() {
+class SPExampleFragment : Fragment(), SPBottomSheetResultListener<String> {
+
+    private var dismiss: (String) -> Unit = {}
+
+    private val binding by lazy {
+        SpExampleFragmentLayoutBinding.inflate(LayoutInflater.from(context))
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.sp_example_fragment_layout, container, false)
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.saveButton.onClick {
+            dismiss("Closed")
+        }
+    }
+
+    override fun setBottomSheetResult(listener: (String) -> Unit) {
+        dismiss = listener
     }
 }
