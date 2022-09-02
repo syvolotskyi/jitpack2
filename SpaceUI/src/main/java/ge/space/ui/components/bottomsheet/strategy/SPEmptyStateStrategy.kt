cf78253f6f66445f85a80029f60b3eba4b5e2c91
v.syvolotskyi <v.syvolotskyi@space.ge>
2022-09-02 18:31:15 +0300

    # Added bottom sheet dialog with bottom button

---
+++ SpaceUI/src/main/java/ge/space/ui/components/bottomsheet/strategy/SPEmptyStateStrategy.kt
@@ -0,0 +1,17 @@
+package ge.space.ui.components.bottomsheet.strategy
+
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+
+/**
+ * Strategy realization of [SPBottomSheetStrategy].
+ * Use it when you don't need to create a container.
+ *
+ */
+class SPEmptyStateStrategy<Data> : SPBottomSheetStrategy<Data> {
+    override fun onCreate(
+        fm: FragmentManager,
+        container: ViewGroup,
+        dismissEvent: (Data?) -> Unit
+    ) = Unit
+}
--- app/src/main/java/ge/space/design/ui_components/text_fields/dropdown/SPDropdownComponent.kt
+++ app/src/main/java/ge/space/design/ui_components/text_fields/dropdown/SPDropdownComponent.kt
@@ -24,6 +24,7 @@ import ge.space.ui.components.text_fields.input.dropdown.SPTextFieldDropdown
 import ge.space.ui.components.text_fields.input.dropdown.data.SPDropdownItemModel
 import ge.space.ui.components.text_fields.input.dropdown.data.SPOnDropdownBind
 import ge.space.ui.util.extension.EMPTY_TEXT
+import ge.space.ui.util.extension.onClick
 import ge.space.ui.util.view_factory.SPViewFactory.Companion.createView
 import ge.space.ui.util.view_factory.component_type.chip.empty.SPDefaultEmptyChipData
 
@@ -63,6 +64,10 @@ class SPDropdownComponent : ShowCaseComponent {
             )
 
             dropdowns.add(simpleDropdown)
+
+            layoutBinding.buttonBottomSheet.onClick {
+                showButtonBottomSheet(environment.requireFragmentActivity())
+            }
 
             layoutBinding.cbMandatory.setOnCheckedChangeListener { _, isChecked ->
                 dropdowns.forEach { it.inputMandatory = isChecked }
@@ -141,18 +146,13 @@ class SPDropdownComponent : ShowCaseComponent {
 
             val adapter =
                 SPBottomSheetAdapter<SpLangItemLayoutBinding, SPDropdownItemModel>().setup {
-                    onCreate {
-                        SpLangItemLayoutBinding.inflate(LayoutInflater.from(view.context))
-                    }
+                    onCreate { SpLangItemLayoutBinding.inflate(LayoutInflater.from(view.context)) }
                     onBind { binding, item, _ ->
-
                         binding.radio.setData(
                             item.item.value,
                             item.item.viewData?.createView(view.context)
                         )
                         binding.radio.isChecked = item.isSelected
-
-
                     }
                 }
 
@@ -201,5 +201,17 @@ class SPDropdownComponent : ShowCaseComponent {
                 Toast.makeText(view.context, it, Toast.LENGTH_SHORT).show()
             }
             .build()
+
+        private fun showButtonBottomSheet(context: FragmentActivity) {
+            SPBottomSheetBuilder<String>()
+                .setTitle(context.getString(R.string.title_default_items))
+                .setDescription(context.getString(R.string.example_text))
+                .setBottomButton(context.getString(R.string.show_btn)) {
+                    Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+                    it.dismiss()
+                }
+                .build()
+                .show(context)
+        }
     }
 }
