ce5725bd79d5c1b393198ec5567064f5755ad24d
v.syvolotskyi <v.syvolotskyi@space.ge>
2022-09-26 10:13:53 +0300

    #fixed adding programaticaly layout params

--- SpaceUI/src/main/java/ge/space/ui/components/controls/radio/pill/SPPillItem.kt
+++ SpaceUI/src/main/java/ge/space/ui/components/controls/radio/pill/SPPillItem.kt
@@ -7,6 +7,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
+import androidx.appcompat.widget.TintTypedArray.obtainStyledAttributes
 import androidx.core.content.withStyledAttributes
 import ge.space.spaceui.R
 import ge.space.spaceui.databinding.SpPillLayoutBinding
@@ -137,5 +138,4 @@ class SPPillItem @JvmOverloads constructor(
             applyPillStyledAttributes()
         }
     }
-
 }
