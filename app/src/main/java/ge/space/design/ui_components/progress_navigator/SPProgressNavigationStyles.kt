cc537dc4f6b3217d074ca9e766eb6fc1fe8f3923
nikoloz.khidesheli <nikoloz.khidesheli>
2022-11-04 12:08:36 +0400

    # Little impr

--- app/src/main/java/ge/space/design/ui_components/progress_navigator/SPProgressNavigationStyles.kt
+++ app/src/main/java/ge/space/design/ui_components/progress_navigator/SPProgressNavigationStyles.kt
@@ -5,12 +5,8 @@ import ge.space.ui.components.progress_navigator.SPProgressNavigatorData
 
 object SPProgressNavigationStyles {
     val list = listOf(
-        SPProgressNavigatorData("Step 1", R.drawable.ic_calendar_24_regular, "Step 1 - Done!"),
-        SPProgressNavigatorData("Step 2", R.drawable.ic_charity_24_regular, "Step 2 - Done!"),
-        SPProgressNavigatorData(
-            "Step 3",
-            R.drawable.ic_clock_backward_24_regular,
-            "Step 3 - Done!"
-        ),
+        SPProgressNavigatorData("Step 1", "Step 1 - Done!", R.drawable.ic_calendar_24_regular),
+        SPProgressNavigatorData("Step 2", "Step 2 - Done!", R.drawable.ic_charity_24_regular),
+        SPProgressNavigatorData("Step 3", "Step 3 - Done!", R.drawable.ic_clock_backward_24_regular),
     )
 }
